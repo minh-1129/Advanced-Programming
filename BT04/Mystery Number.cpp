ec141b8e18e120ea938575f31774b5da7dd696e2
minhuet <109837037+minh-1129@users.noreply.github.com>
2023-03-02 07:57:16 +0700

    Create Mystery Number.cpp

---
+++ BT04/Mystery Number.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    int b[n+1];
+    for(int i = 0; i < n; i++)
+        cin>>a[i];
+    for(int i = 0; i <=n; i++)
+        cin>>b[i];
+    sort(a,a+n);
+    sort(b,b+n+1);
+    for(int i = 0; i < n; i++){
+        if(a[i] != b[i]){
+            cout<<b[i];
+            return 0;
+        }
+    }
+    cout<<b[n];
+    return 0;
+}
+
